90e077df85300853f4f192dfdf2a5596303cfcaa
Kevin <104107277+KevinSantander@users.noreply.github.com>
2022-05-08 20:08:41 -0300

    Mayor-de-Tres

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,14 @@
-let numero1 = document.getElementById("numero1");
-let numero2 = document.getElementById("numero1");
-let numero3 = document.getElementById("numero3");
 let btnCalcular = document.getElementById("calcular");
 
-btnCalcular.addEventListener("click"), () => {
-  
-  let numero1: number = (dato1.value);
-  let numero2: number = (dato2.value);
-  let numero3: number = (dato3.value);
-  
-  if (numero1 > numero2&&numero1 > numero3)  {
-     console.log("El numero" +numero1+ "es el mayor");
-    }else if (numero2 > numero1&&numero2 > numero3) {
-      console.log("El numero" +numero2+"es el mayor");
-      }else if (numero3 > numero1&&numero3 > numero2) {
-      console.log("El numero"+numero3+"es el mayor");
-    }
-}
-);
-
+btnCalcular.addEventListener("click", () => {
+  let primerNumero = Number(data1.value);
+  let segundoNumero = Number(data2.value);
+  let tercerNumero = Number(data3.value);
+  if (primerNumero > segundoNumero && primerNumero > tercerNumero) {
+    console.log(primerNumero + " es el mayor");
+  } else {
+    if (segundoNumero > primerNumero && segundoNumero > tercerNumero) {
+      console.log(segundoNumero + " es el mayor");
+    } else console.log(tercerNumero + " es el mayor");
+  }
+});
